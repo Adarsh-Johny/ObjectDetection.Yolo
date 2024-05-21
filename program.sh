6f601ce0ac4a49a4ec3ebf44cf8ea9a72c03783b
Adarsh <Adarshjohny02@gmail.com>
2024-05-21 14:48:06 +0200

    execution changes

--- program.sh
+++ program.sh
@@ -24,7 +24,7 @@ SBATCH --cpus-per-task 16
 
 echo "Now processing task id:: ${SLURM_JOB_ID} on ${SLURMD_NODENAME}"
 mkdir "log_${SLURM_JOB_ID}"
-python yolov5-traffic-monitoring/train.py --img 640 --batch 16 --epochs 1 --data depth_images.yaml --cfg yolov5-traffic-monitoring/models/custom_yolov5s.yaml --weights yolov5s.pt --workers 2 --name kitti_yolov5s > output_${SLURM_JOB_ID}.txt
+python yolov5-traffic-monitoring/train.py --img 640 --batch 16 --epochs 1 --data depth_images.yaml --cfg yolov5-traffic-monitoring/models/custom_yolov5s.yaml --weights yolov5s.pt --workers 4 --name kitti_yolov5s > output_${SLURM_JOB_ID}.txt
 
 echo "finished task with id:: ${SLURM_JOB_ID}"
 # happy end
