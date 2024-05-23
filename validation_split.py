534e6890b37da71570643a864998da6bed158420
Adarsh <Adarshjohny02@gmail.com>
2024-05-24 01:52:55 +0200

    kiti training restructure

--- validation_split.py
+++ validation_split.py
@@ -1,41 +1,36 @@
 import os
 import shutil
 
-# Define paths
-base_dir = "../Project_Files/datasets"
-train_images_dir = os.path.join(base_dir, "images/training")
-train_labels_dir = os.path.join(base_dir, "labels/training")
-val_images_dir = os.path.join(base_dir, "images/validation")
-val_labels_dir = os.path.join(base_dir, "labels/validation")
+class DatasetSplitter:
+    def __init__(self, train_images_dir, train_labels_dir, val_images_dir, val_labels_dir, num_val_files):
+        self.train_images_dir = train_images_dir
+        self.train_labels_dir = train_labels_dir
+        self.val_images_dir = val_images_dir
+        self.val_labels_dir = val_labels_dir
+        self.num_val_files = num_val_files
 
-# Create validation directories if they don't exist
-os.makedirs(val_images_dir, exist_ok=True)
-os.makedirs(val_labels_dir, exist_ok=True)
+        self.create_validation_dirs()
+        self.train_image_files = sorted(os.listdir(self.train_images_dir))
+        self.train_label_files = sorted(os.listdir(self.train_labels_dir))
 
-# Get list of all image and label files
-train_image_files = sorted(os.listdir(train_images_dir))
-train_label_files = sorted(os.listdir(train_labels_dir))
+        assert len(self.train_image_files) == len(self.train_label_files), "Mismatch between number of image and label files"
 
-# Check that the number of image and label files match
-assert len(train_image_files) == len(train_label_files), "Mismatch between number of image and label files"
+        self.val_image_files = self.train_image_files[-self.num_val_files:]
+        self.val_label_files = self.train_label_files[-self.num_val_files:]
 
-# Number of files to move to validation
-num_val_files = 481
+    def create_validation_dirs(self):
+        os.makedirs(self.val_images_dir, exist_ok=True)
+        os.makedirs(self.val_labels_dir, exist_ok=True)
 
-# Get the files to move
-val_image_files = train_image_files[-num_val_files:]
-val_label_files = train_label_files[-num_val_files:]
+    def move_files(self, file_list, src_dir, dest_dir):
+        for file_name in file_list:
+            src_path = os.path.join(src_dir, file_name)
+            dest_path = os.path.join(dest_dir, file_name)
+            shutil.move(src_path, dest_path)
+            print(f"Moved {src_path} to {dest_path}")
 
-# Function to move files
-def move_files(file_list, src_dir, dest_dir):
-    for file_name in file_list:
-        src_path = os.path.join(src_dir, file_name)
-        dest_path = os.path.join(dest_dir, file_name)
-        shutil.move(src_path, dest_path)
-        print(f"Moved {src_path} to {dest_path}")
+    def split(self):
+        self.move_files(self.val_image_files, self.train_images_dir, self.val_images_dir)
+        self.move_files(self.val_label_files, self.train_labels_dir, self.val_labels_dir)
+        print("Dataset split into training and validation sets successfully.")
 
-# Move the files
-move_files(val_image_files, train_images_dir, val_images_dir)
-move_files(val_label_files, train_labels_dir, val_labels_dir)
-
-print("Dataset split into training and validation sets successfully.")
